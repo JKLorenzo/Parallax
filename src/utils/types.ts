a15e6182121778d5bd463589109bf9de1b69ef67
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-08-03 16:05:09 +0800

    feat(types): add guild configs

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,3 +12,53 @@ export type GuildCommandOptions = {
     roles?: CommandPermissionData;
   };
 };
+
+export type GuildConfig = {
+  dedicated?: DedicatedConfig;
+  free_game?: FreeGameConfig;
+  game?: GameConfig;
+  nsfw?: NSFWConfig;
+  play?: PlayConfig;
+  streaming?: StreamingConfig;
+};
+
+export type DedicatedConfig = {
+  hoisted?: boolean;
+  text_category?: Snowflake;
+  voice_category?: Snowflake;
+  reference_role?: Snowflake;
+};
+
+export type FreeGameConfig = {
+  free_games_channel?: Snowflake;
+  epic_role?: Snowflake;
+  gog_role?: Snowflake;
+  ps_role?: Snowflake;
+  steam_role?: Snowflake;
+  uplay_role?: Snowflake;
+  wii_role?: Snowflake;
+  xbox_role?: Snowflake;
+};
+
+export type GameConfig = {
+  enabled?: boolean;
+  mentionable?: boolean;
+  invite_channel?: Snowflake;
+  reference_role?: Snowflake;
+};
+
+export type NSFWConfig = {
+  nsfw_channels?: Snowflake[];
+  nsfw_role?: Snowflake;
+};
+
+export type PlayConfig = {
+  enabled?: boolean;
+  mentionable?: boolean;
+  reference_role?: Snowflake;
+};
+
+export type StreamingConfig = {
+  enabled?: boolean;
+  streaming_role?: Snowflake;
+};
