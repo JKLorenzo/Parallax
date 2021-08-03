91bad61c1c2154d5f348c056258151f81c22e74c
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-08-03 16:10:13 +0800

    feat: add component structure

---
+++ src/structures/component.ts
@@ -0,0 +1,39 @@
+import {
+  MessageActionRow,
+  MessageActionRowComponent,
+  MessageActionRowOptions,
+  MessageComponentInteraction,
+} from 'discord.js';
+
+type ComponentData = {
+  name: string;
+  options: MessageActionRowOptions[];
+};
+
+export default abstract class Component {
+  private _name: string;
+  private _options: MessageActionRowOptions[];
+
+  constructor(data: ComponentData) {
+    this._name = data.name;
+    this._options = data.options;
+  }
+
+  abstract exec(interaction: MessageComponentInteraction, customId: string): Promise<void>;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get options(): MessageActionRowOptions[] {
+    return this._options.map(
+      action_row =>
+        new MessageActionRow({
+          components: action_row.components.map(component => ({
+            ...component,
+            customId: `${this.name}__${(component as MessageActionRowComponent).customId}`,
+          })) as MessageActionRowComponent[],
+        }),
+    );
+  }
+}
