15dbfcad78aa5d8072fd26f4602038c98fd1ed5a
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-08-03 19:31:05 +0800

    feat(functions): update getFiles()

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function getFiles(dir: string): string[] {
+  if (!fs.existsSync(dir)) return [];
   return fs.readdirSync(dir).reduce<string[]>((list, file) => {
     const name = path.join(dir, file);
     const isDir = fs.statSync(name).isDirectory();
