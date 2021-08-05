599ff934c09fa96e05dde355d7dc167d6715ed76
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-08-05 20:03:32 +0800

    refactor(database): rename _cache to _config

--- src/modules/database.ts
+++ src/modules/database.ts
@@ -10,7 +10,7 @@ import {
   StreamingConfig,
 } from '../utils/types.js';
 
-const _cache = new Collection<Snowflake, GuildConfig>();
+const _config = new Collection<Snowflake, GuildConfig>();
 const mongoClient = new mongodb.MongoClient(process.env.DB_URI!);
 
 export async function connectDb(): Promise<void> {
@@ -18,10 +18,10 @@ export async function connectDb(): Promise<void> {
 }
 
 export async function getDedicatedConfig(guildId: Snowflake): Promise<DedicatedConfig | undefined> {
-  if (!_cache.get(guildId)?.dedicated) {
+  if (!_config.get(guildId)?.dedicated) {
     const result = await mongoClient.db(guildId).collection('config').findOne({ _id: 'dedicated' });
 
-    _cache.set(guildId, {
+    _config.set(guildId, {
       dedicated: {
         hoisted: result?.hoisted,
         reference_role: result?.reference_role,
@@ -31,7 +31,7 @@ export async function getDedicatedConfig(guildId: Snowflake): Promise<DedicatedC
     });
   }
 
-  return _cache.get(guildId)?.dedicated;
+  return _config.get(guildId)?.dedicated;
 }
 
 export async function updateDedicatedConfig(
@@ -40,13 +40,13 @@ export async function updateDedicatedConfig(
 ): Promise<void> {
   if (Object.keys(data).length === 0) return;
 
-  const config = _cache.get(guildId) ?? {};
+  const config = _config.get(guildId) ?? {};
   if (!config.dedicated) config.dedicated = {};
   if ('hoisted' in data) config.dedicated.hoisted = data.hoisted;
   if ('reference_role' in data) config.dedicated.reference_role = data.reference_role;
   if ('text_category' in data) config.dedicated.text_category = data.text_category;
   if ('voice_category' in data) config.dedicated.voice_category = data.voice_category;
-  _cache.set(guildId, config);
+  _config.set(guildId, config);
 
   await mongoClient
     .db(guildId)
@@ -55,10 +55,10 @@ export async function updateDedicatedConfig(
 }
 
 export async function getFreeGameConfig(guildId: Snowflake): Promise<FreeGameConfig | undefined> {
-  if (!_cache.get(guildId)?.free_game) {
+  if (!_config.get(guildId)?.free_game) {
     const result = await mongoClient.db(guildId).collection('config').findOne({ _id: 'free_game' });
 
-    _cache.set(guildId, {
+    _config.set(guildId, {
       free_game: {
         free_games_channel: result?.free_games_channel,
         epic_role: result?.epic_role,
@@ -72,7 +72,7 @@ export async function getFreeGameConfig(guildId: Snowflake): Promise<FreeGameCon
     });
   }
 
-  return _cache.get(guildId)?.free_game;
+  return _config.get(guildId)?.free_game;
 }
 
 export async function updateFreeGameConfig(
@@ -81,7 +81,7 @@ export async function updateFreeGameConfig(
 ): Promise<void> {
   if (Object.keys(data).length === 0) return;
 
-  const config = _cache.get(guildId) ?? {};
+  const config = _config.get(guildId) ?? {};
   if (!config.free_game) config.free_game = {};
   if ('free_games_channel' in data) config.free_game.free_games_channel = data.free_games_channel;
   if ('epic_role' in data) config.free_game.epic_role = data.epic_role;
@@ -91,7 +91,7 @@ export async function updateFreeGameConfig(
   if ('uplay_role' in data) config.free_game.uplay_role = data.uplay_role;
   if ('wii_role' in data) config.free_game.wii_role = data.wii_role;
   if ('xbox_role' in data) config.free_game.xbox_role = data.xbox_role;
-  _cache.set(guildId, config);
+  _config.set(guildId, config);
 
   await mongoClient
     .db(guildId)
@@ -100,32 +100,34 @@ export async function updateFreeGameConfig(
 }
 
 export async function getGameConfig(guildId: Snowflake): Promise<GameConfig | undefined> {
-  if (!_cache.get(guildId)?.game) {
+  if (!_config.get(guildId)?.game) {
     const result = await mongoClient.db(guildId).collection('config').findOne({ _id: 'game' });
 
-    _cache.set(guildId, {
+    _config.set(guildId, {
       game: {
         enabled: result?.enabled,
         mentionable: result?.mentionable,
+        color: result?.color,
         invite_channel: result?.invite_channel,
         reference_role: result?.reference_role,
       },
     });
   }
 
-  return _cache.get(guildId)?.game;
+  return _config.get(guildId)?.game;
 }
 
 export async function updateGameConfig(guildId: Snowflake, data: GameConfig): Promise<void> {
   if (Object.keys(data).length === 0) return;
 
-  const config = _cache.get(guildId) ?? {};
+  const config = _config.get(guildId) ?? {};
   if (!config.game) config.game = {};
   if ('enabled' in data) config.game.enabled = data.enabled;
   if ('mentionable' in data) config.game.mentionable = data.mentionable;
+  if ('color' in data) config.game.color = data.color;
   if ('invite_channel' in data) config.game.invite_channel = data.invite_channel;
   if ('reference_role' in data) config.game.reference_role = data.reference_role;
-  _cache.set(guildId, config);
+  _config.set(guildId, config);
 
   await mongoClient
     .db(guildId)
@@ -134,10 +136,10 @@ export async function updateGameConfig(guildId: Snowflake, data: GameConfig): Pr
 }
 
 export async function getNSFWConfig(guildId: Snowflake): Promise<NSFWConfig | undefined> {
-  if (!_cache.get(guildId)?.nsfw) {
+  if (!_config.get(guildId)?.nsfw) {
     const result = await mongoClient.db(guildId).collection('config').findOne({ _id: 'nsfw' });
 
-    _cache.set(guildId, {
+    _config.set(guildId, {
       nsfw: {
         nsfw_channels: result?.nsfw_channels,
         nsfw_role: result?.nsfw_role,
@@ -145,17 +147,17 @@ export async function getNSFWConfig(guildId: Snowflake): Promise<NSFWConfig | un
     });
   }
 
-  return _cache.get(guildId)?.nsfw;
+  return _config.get(guildId)?.nsfw;
 }
 
 export async function updateNSFWConfig(guildId: Snowflake, data: NSFWConfig): Promise<void> {
   if (Object.keys(data).length === 0) return;
 
-  const config = _cache.get(guildId) ?? {};
+  const config = _config.get(guildId) ?? {};
   if (!config.nsfw) config.nsfw = {};
   if ('nsfw_channels' in data) config.nsfw.nsfw_channels = data.nsfw_channels;
   if ('nsfw_role' in data) config.nsfw.nsfw_role = data.nsfw_role;
-  _cache.set(guildId, config);
+  _config.set(guildId, config);
 
   await mongoClient
     .db(guildId)
@@ -164,10 +166,10 @@ export async function updateNSFWConfig(guildId: Snowflake, data: NSFWConfig): Pr
 }
 
 export async function getPlayConfig(guildId: Snowflake): Promise<GameConfig | undefined> {
-  if (!_cache.get(guildId)?.play) {
+  if (!_config.get(guildId)?.play) {
     const result = await mongoClient.db(guildId).collection('config').findOne({ _id: 'play' });
 
-    _cache.set(guildId, {
+    _config.set(guildId, {
       play: {
         enabled: result?.enabled,
         mentionable: result?.mentionable,
@@ -176,18 +178,18 @@ export async function getPlayConfig(guildId: Snowflake): Promise<GameConfig | un
     });
   }
 
-  return _cache.get(guildId)?.play;
+  return _config.get(guildId)?.play;
 }
 
 export async function updatePlayConfig(guildId: Snowflake, data: PlayConfig): Promise<void> {
   if (Object.keys(data).length === 0) return;
 
-  const config = _cache.get(guildId) ?? {};
+  const config = _config.get(guildId) ?? {};
   if (!config.play) config.play = {};
   if ('enabled' in data) config.play.enabled = data.enabled;
   if ('mentionable' in data) config.play.mentionable = data.mentionable;
   if ('reference_role' in data) config.play.reference_role = data.reference_role;
-  _cache.set(guildId, config);
+  _config.set(guildId, config);
 
   await mongoClient
     .db(guildId)
@@ -196,10 +198,10 @@ export async function updatePlayConfig(guildId: Snowflake, data: PlayConfig): Pr
 }
 
 export async function getStreamingConfig(guildId: Snowflake): Promise<StreamingConfig | undefined> {
-  if (!_cache.get(guildId)?.streaming) {
+  if (!_config.get(guildId)?.streaming) {
     const result = await mongoClient.db(guildId).collection('config').findOne({ _id: 'streaming' });
 
-    _cache.set(guildId, {
+    _config.set(guildId, {
       streaming: {
         enabled: result?.enabled,
         streaming_role: result?.streaming_role,
@@ -207,7 +209,7 @@ export async function getStreamingConfig(guildId: Snowflake): Promise<StreamingC
     });
   }
 
-  return _cache.get(guildId)?.streaming;
+  return _config.get(guildId)?.streaming;
 }
 
 export async function updateStreamingConfig(
@@ -216,11 +218,11 @@ export async function updateStreamingConfig(
 ): Promise<void> {
   if (Object.keys(data).length === 0) return;
 
-  const config = _cache.get(guildId) ?? {};
+  const config = _config.get(guildId) ?? {};
   if (!config.streaming) config.streaming = {};
   if ('enabled' in data) config.streaming.enabled = data.enabled;
   if ('streaming_role' in data) config.streaming.streaming_role = data.streaming_role;
-  _cache.set(guildId, config);
+  _config.set(guildId, config);
 
   await mongoClient
     .db(guildId)
