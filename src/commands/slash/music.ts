8c2cf10b819c24e09f957d12dd149583e9183a92
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-08-21 22:57:46 +0800

    feat: add music slash command

---
+++ src/commands/slash/music.ts
@@ -0,0 +1,182 @@
+import {
+  AudioPlayerStatus,
+  AudioResource,
+  entersState,
+  joinVoiceChannel,
+  VoiceConnectionStatus,
+} from '@discordjs/voice';
+import { CommandInteraction, Guild, GuildMember, Snowflake } from 'discord.js';
+import { Track, MusicSubscription } from '../../managers/music.js';
+import Command from '../../structures/command.js';
+
+const subscriptions = new Map<Snowflake, MusicSubscription>();
+
+export default class Music extends Command {
+  constructor() {
+    super('guild', {
+      name: 'music',
+      description: 'Contains all the music commands of this bot.',
+      type: 'CHAT_INPUT',
+      defaultPermission: true,
+      options: [
+        {
+          name: 'play',
+          description: 'Plays a song on your current voice channel',
+          type: 'SUB_COMMAND',
+          options: [
+            {
+              name: 'song',
+              type: 'STRING',
+              description: 'The URL of the song to play',
+              required: true,
+            },
+          ],
+        },
+        {
+          name: 'skip',
+          description: 'Skip to the next song in the queue',
+          type: 'SUB_COMMAND',
+        },
+        {
+          name: 'queue',
+          description: 'See the music queue',
+          type: 'SUB_COMMAND',
+        },
+        {
+          name: 'pause',
+          description: 'Pauses the song that is currently playing',
+          type: 'SUB_COMMAND',
+        },
+        {
+          name: 'resume',
+          description: 'Resume playback of the current song',
+          type: 'SUB_COMMAND',
+        },
+        {
+          name: 'leave',
+          description: 'Leave the voice channel',
+          type: 'SUB_COMMAND',
+        },
+      ],
+    });
+  }
+
+  async exec(interaction: CommandInteraction): Promise<unknown> {
+    const command = interaction.options.getSubcommand();
+    const guild = interaction.guild as Guild;
+    const member = interaction.member as GuildMember;
+    const channel = member.voice.channel;
+
+    let subscription = subscriptions.get(guild.id);
+
+    if (command === 'play') {
+      await interaction.deferReply();
+      // Extract the video URL from the command
+      const url = interaction.options.getString('song', true);
+
+      // If a connection to the guild doesn't already exist and the user is in a voice channel, join that channel
+      // and create a subscription.
+      if (!subscription && channel) {
+        subscription = new MusicSubscription(
+          joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+          }),
+        );
+        subscription.voiceConnection.on('error', console.warn);
+        subscriptions.set(guild.id, subscription);
+      }
+
+      // If there is no subscription, tell the user they need to join a channel.
+      if (!subscription) {
+        return interaction.followUp('Join a voice channel and then try that again!');
+      }
+
+      // Make sure the connection is ready before processing the user's request
+      try {
+        await entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 20e3);
+      } catch (error) {
+        console.warn(error);
+        return interaction.followUp(
+          'Failed to join voice channel within 20 seconds, please try again later!',
+        );
+      }
+
+      try {
+        // Attempt to create a Track from the user's video URL
+        const track = await Track.from(url, {
+          onStart() {
+            interaction.followUp({ content: 'Now playing!', ephemeral: true }).catch(console.warn);
+          },
+          onFinish() {
+            interaction.followUp({ content: 'Now finished!', ephemeral: true }).catch(console.warn);
+          },
+          onError(error) {
+            console.warn(error);
+            interaction
+              .followUp({ content: `Error: ${error.message}`, ephemeral: true })
+              .catch(console.warn);
+          },
+        });
+        // Enqueue the track and reply a success message to the user
+        subscription.enqueue(track);
+        await interaction.followUp(`Enqueued **${track.title}**`);
+      } catch (error) {
+        console.warn(error);
+        await interaction.reply('Failed to play track, please try again later!');
+      }
+    } else if (command === 'skip') {
+      if (subscription) {
+        // Calling .stop() on an AudioPlayer causes it to transition into the Idle state. Because of a state transition
+        // listener defined in music/subscription.ts, transitions into the Idle state mean the next track from the queue
+        // will be loaded and played.
+        subscription.audioPlayer.stop();
+        await interaction.reply('Skipped song!');
+      } else {
+        await interaction.reply('Not playing in this server!');
+      }
+    } else if (command === 'queue') {
+      // Print out the current queue, including up to the next 5 tracks to be played.
+      if (subscription) {
+        const current =
+          subscription.audioPlayer.state.status === AudioPlayerStatus.Idle
+            ? `Nothing is currently playing!`
+            : `Playing **${
+                (subscription.audioPlayer.state.resource as AudioResource<Track>).metadata.title
+              }**`;
+
+        const queue = subscription.queue
+          .slice(0, 5)
+          .map((track, index) => `${index + 1}) ${track.title}`)
+          .join('\n');
+
+        await interaction.reply(`${current}\n\n${queue}`);
+      } else {
+        await interaction.reply('Not playing in this server!');
+      }
+    } else if (command === 'pause') {
+      if (subscription) {
+        subscription.audioPlayer.pause();
+        await interaction.reply({ content: `Paused!`, ephemeral: true });
+      } else {
+        await interaction.reply('Not playing in this server!');
+      }
+    } else if (interaction.commandName === 'resume') {
+      if (subscription) {
+        subscription.audioPlayer.unpause();
+        await interaction.reply({ content: `Unpaused!`, ephemeral: true });
+      } else {
+        await interaction.reply('Not playing in this server!');
+      }
+    } else if (interaction.commandName === 'leave') {
+      if (subscription) {
+        subscription.voiceConnection.destroy();
+        subscriptions.delete(guild.id);
+        await interaction.reply({ content: `Left channel!`, ephemeral: true });
+      } else {
+        await interaction.reply('Not playing in this server!');
+      }
+    }
+  }
+}
