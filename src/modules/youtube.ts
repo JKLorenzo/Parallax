7a7434618c2bbb73931230602d81e4f06c3c49f8
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-10-02 02:01:29 +0800

    feat(youtube): ignore term case for caching

--- src/modules/youtube.ts
+++ src/modules/youtube.ts
@@ -1,14 +1,9 @@
-import youtubeSearch, { YouTubeSearchOptions, YouTubeSearchResults } from 'youtube-search';
+import youtubeSearch, { YouTubeSearchResults } from 'youtube-search';
 import ytdl_core from 'ytdl-core';
 const { getInfo } = ytdl_core;
 
 const _infoCache = new Map<string, ytdl_core.videoInfo>();
 const _searchCache = new Map<string, YouTubeSearchResults>();
-
-const options: YouTubeSearchOptions = {
-  key: process.env.GOOGLE_KEY,
-  maxResults: 1,
-};
 
 export async function getYouTubeInfo(url: string): Promise<ytdl_core.videoInfo | undefined> {
   const id = url.trim();
@@ -23,12 +18,15 @@ export async function getYouTubeInfo(url: string): Promise<ytdl_core.videoInfo |
 }
 
 export async function searchYouTube(term: string): Promise<YouTubeSearchResults | undefined> {
-  const id = term.trim();
+  const id = term.toLowerCase().trim();
 
   const cached = _searchCache.get(id);
   if (cached) return cached;
 
-  const data = await youtubeSearch(id, options);
+  const data = await youtubeSearch(id, {
+    key: process.env.GOOGLE_KEY,
+    maxResults: 1,
+  });
 
   const result = data.results[0];
   if (result) _searchCache.set(id, result);
