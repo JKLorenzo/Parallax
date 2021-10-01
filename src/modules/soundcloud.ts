46ea1d286c13a24028807d309eddee767c5c089c
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-10-02 02:25:27 +0800

    feat(soundcloud): add search support

--- src/modules/soundcloud.ts
+++ src/modules/soundcloud.ts
@@ -1,8 +1,10 @@
-import { Client, Playlist, Song } from 'soundcloud-scraper';
+import { Client, Playlist, SearchResult, Song } from 'soundcloud-scraper';
+
 const soundcloud = new Client();
 
 const _trackCache = new Map<string, Song>();
 const _playlistCache = new Map<string, Playlist>();
+const _searchCache = new Map<string, SearchResult>();
 
 function removeQueries(url: string): string {
   return url.split('?')[0];
@@ -30,4 +32,18 @@ export async function getSoundCloudPlaylist(url: string): Promise<Playlist | und
   if (playlist) _playlistCache.set(id, playlist);
 
   return playlist;
+}
+
+export async function searchSoundCloud(term: string): Promise<SearchResult | undefined> {
+  const id = term.toLowerCase().trim();
+
+  const cached = _searchCache.get(id);
+  if (cached) return cached;
+
+  const data = await soundcloud.search(term, 'track');
+
+  const result = data[0];
+  if (result) _searchCache.set(id, result);
+
+  return result;
 }
