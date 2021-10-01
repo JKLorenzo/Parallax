45b7a032aca46aad82d91a86e864200ca95c56a5
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-10-02 00:19:21 +0800

    feat(music): use track and subscription structures

--- src/managers/music.ts
+++ src/managers/music.ts
@@ -1,371 +1,40 @@
 import {
-  AudioPlayer,
   AudioPlayerStatus,
   AudioResource,
-  createAudioPlayer,
-  createAudioResource,
-  demuxProbe,
   entersState,
   joinVoiceChannel,
-  VoiceConnection,
-  VoiceConnectionDisconnectReason,
   VoiceConnectionStatus,
 } from '@discordjs/voice';
 import {
   CommandInteraction,
   Guild,
   GuildMember,
-  Message,
   MessageComponentInteraction,
   Snowflake,
   TextChannel,
 } from 'discord.js';
 import fetch from 'node-fetch';
-import { raw as ytdl } from 'youtube-dl-exec';
 import ytdl_core from 'ytdl-core';
-import { getComponent } from './interaction.js';
 import { getSoundCloudPlaylist, getSoundCloudTrack } from '../modules/soundcloud.js';
 import { getPlaylist, getTrack } from '../modules/spotify.js';
 import { searchYouTube } from '../modules/youtube.js';
-import { hasAll, hasAny, parseHTML, sleep } from '../utils/functions.js';
+import Subscription from '../structures/subscription.js';
+import Track from '../structures/track.js';
+import { hasAny, parseHTML } from '../utils/functions.js';
 const { getInfo } = ytdl_core;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = () => {};
+const _subscriptions = new Map<Snowflake, Subscription>();
 
-const _subscriptions = new Map<Snowflake, MusicSubscription>();
-
-interface TrackData {
-  query: string;
-  title?: string;
-  image?: string;
-  onStart: () => void;
-  onFinish: () => void;
-  onError: (error: Error) => void;
-}
-
-export function getSubscription(guild_id: Snowflake): MusicSubscription | undefined {
+export function getSubscription(guild_id: Snowflake): Subscription | undefined {
   return _subscriptions.get(guild_id);
 }
 
-export function setSubscription(guild_id: Snowflake, subscription: MusicSubscription): void {
+export function setSubscription(guild_id: Snowflake, subscription: Subscription): void {
   _subscriptions.set(guild_id, subscription);
 }
 
 export function deleteSubscription(guild_id: Snowflake): void {
   _subscriptions.delete(guild_id);
-}
-
-export class Track implements TrackData {
-  query: string;
-  title?: string;
-  image?: string;
-  onStart: () => void;
-  onFinish: () => void;
-  onError: (error: Error) => void;
-
-  constructor(channel: TextChannel, query: string, title?: string, image?: string) {
-    this.query = query;
-    this.title = title;
-    this.image = image;
-
-    let message: Message | undefined;
-
-    this.onStart = () => {
-      this.onStart = noop;
-      if (channel && !message) {
-        const voice_channel = channel.guild.me?.voice.channel;
-        const subscription = getSubscription(channel.guildId);
-        const nextTrack = subscription?.queue.at(0);
-
-        channel
-          .send({
-            embeds: [
-              {
-                author: { name: 'Parallax Music Player: Now Playing' },
-                title: this.title,
-                description: nextTrack ? `Up Next: ${nextTrack.title}` : '',
-                footer: {
-                  text: `Channel: ${voice_channel?.name ?? 'Unknown'}  |  Region: ${
-                    voice_channel?.rtcRegion
-                      ?.split(' ')
-                      .map(s => `${s.charAt(0).toUpperCase()}${s.slice(1)}`) ?? 'Automatic'
-                  }  |  Bitrate: ${
-                    voice_channel ? `${voice_channel.bitrate / 1000}kbps` : 'Unknown'
-                  }`,
-                },
-                thumbnail: { url: this.image },
-                color: 'GREEN',
-              },
-            ],
-            components: getComponent('music'),
-          })
-          .then(msg => (message = msg))
-          .catch(console.warn);
-      }
-    };
-
-    this.onFinish = () => {
-      this.onFinish = noop;
-      if (message && message.editable) {
-        message
-          .edit({
-            embeds: [
-              message.embeds[0]
-                .setAuthor('Parallax Music Player: Previously Played')
-                .setColor('YELLOW'),
-            ],
-            components: [],
-          })
-          .catch(console.warn);
-        setTimeout(() => {
-          if (message && message.deletable) message.delete().catch(console.warn);
-        }, 10000);
-      }
-    };
-
-    this.onError = error => {
-      this.onError = noop;
-      console.warn(error);
-      if (message && message.deletable) message.delete().catch(console.warn);
-    };
-  }
-
-  async createAudioResource(): Promise<AudioResource<Track>> {
-    if (!hasAny(this.query, 'http') || hasAny(this.query, 'youtube.com')) {
-      let url;
-      if (hasAny(this.query, 'http')) {
-        url = this.query;
-      } else {
-        const data = await searchYouTube(this.query);
-        if (!data) throw new Error('No track found.');
-
-        const title = parseHTML(data.title).trim();
-        const author = parseHTML(data.channelTitle).trim();
-
-        url = data.link;
-        if (!this.title) this.title = `${title} by ${author}`;
-        if (!this.image) this.image = data.thumbnails.default?.url;
-      }
-
-      if (!this.title || !this.image) {
-        const info = await getInfo(url);
-        if (!info) throw new Error('No track info found.');
-
-        const title = parseHTML(info.videoDetails.title).trim();
-        const author = parseHTML(info.videoDetails.ownerChannelName).trim();
-
-        if (!this.title) this.title = `${title} by ${author}`;
-        if (!this.image) this.image = info.thumbnail_url;
-      }
-
-      const process = ytdl(
-        url,
-        {
-          o: '-',
-          q: '',
-          f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
-          r: '100K',
-        },
-        { stdio: ['ignore', 'pipe', 'ignore'] },
-      );
-
-      return new Promise((resolve, reject) => {
-        if (!process.stdout) return reject(new Error('No stdout'));
-
-        const stream = process.stdout;
-        const onError = (error: Error) => {
-          if (!process.killed) process.kill();
-          stream.resume();
-          reject(error);
-        };
-
-        process
-          .once('spawn', () => {
-            demuxProbe(stream)
-              .then(probe =>
-                resolve(
-                  createAudioResource(probe.stream, { metadata: this, inputType: probe.type }),
-                ),
-              )
-              .catch(onError);
-          })
-          .catch(onError);
-      });
-    } else if (hasAll(this.query, ['http', 'soundcloud'])) {
-      const song = await getSoundCloudTrack(this.query);
-      if (!song) throw new Error('Track not found.');
-
-      const title = parseHTML(song.title).trim();
-      const author = parseHTML(song.author.name).trim();
-
-      if (!this.title) this.title = `${title} by ${author}`;
-      if (!this.image) this.image = song.thumbnail;
-
-      return new Promise((resolve, reject) => {
-        song.downloadProgressive().then(stream => {
-          const onError = (error: Error) => {
-            if (!stream.destroyed) stream.destroy();
-            stream.resume();
-            reject(error);
-          };
-
-          demuxProbe(stream)
-            .then(probe =>
-              resolve(createAudioResource(probe.stream, { metadata: this, inputType: probe.type })),
-            )
-            .catch(onError);
-        });
-      });
-    } else {
-      throw new Error('Unsupported Format');
-    }
-  }
-}
-
-export class MusicSubscription {
-  readonly voiceConnection: VoiceConnection;
-  readonly audioPlayer: AudioPlayer;
-  queue: Track[];
-  queueLock = false;
-  readyLock = false;
-
-  constructor(voiceConnection: VoiceConnection) {
-    this.voiceConnection = voiceConnection;
-    this.audioPlayer = createAudioPlayer();
-    this.queue = [];
-
-    this.voiceConnection.on('stateChange', async (_, newState) => {
-      if (newState.status === VoiceConnectionStatus.Disconnected) {
-        if (
-          newState.reason === VoiceConnectionDisconnectReason.WebSocketClose &&
-          newState.closeCode === 4014
-        ) {
-          /*
-						If the WebSocket closed with a 4014 code, this means that we should not manually attempt to reconnect,
-						but there is a chance the connection will recover itself if the reason of the disconnect was due to
-						switching voice channels. This is also the same code for the bot being kicked from the voice channel,
-						so we allow 5 seconds to figure out which scenario it is. If the bot has been kicked, we should destroy
-						the voice connection.
-					*/
-          try {
-            await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
-            // Probably moved voice channel
-          } catch {
-            this.voiceConnection.destroy();
-            // Probably removed from voice channel
-          }
-        } else if (this.voiceConnection.rejoinAttempts < 5) {
-          /*
-						The disconnect in this case is recoverable, and we also have <5 repeated attempts so we will reconnect.
-					*/
-          await sleep((this.voiceConnection.rejoinAttempts + 1) * 5_000);
-          this.voiceConnection.rejoin();
-        } else {
-          /*
-						The disconnect in this case may be recoverable, but we have no more remaining attempts - destroy.
-					*/
-          this.voiceConnection.destroy();
-        }
-      } else if (newState.status === VoiceConnectionStatus.Destroyed) {
-        /*
-					Once destroyed, stop the subscription
-				*/
-        this.stop({ force: true });
-      } else if (
-        !this.readyLock &&
-        (newState.status === VoiceConnectionStatus.Connecting ||
-          newState.status === VoiceConnectionStatus.Signalling)
-      ) {
-        /*
-					In the Signalling or Connecting states, we set a 20 second time limit for the connection to become ready
-					before destroying the voice connection. This stops the voice connection permanently existing in one of these
-					states.
-				*/
-        this.readyLock = true;
-        try {
-          await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, 20_000);
-        } catch {
-          if (this.voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) {
-            this.voiceConnection.destroy();
-          }
-        } finally {
-          this.readyLock = false;
-        }
-      }
-    });
-
-    // Configure audio player
-    this.audioPlayer.on('stateChange', (oldState, newState) => {
-      if (
-        newState.status === AudioPlayerStatus.Idle &&
-        oldState.status !== AudioPlayerStatus.Idle
-      ) {
-        // If the Idle state is entered from a non-Idle state, it means that an audio resource has finished playing.
-        // The queue is then processed to start playing the next track, if one is available.
-        (oldState.resource as AudioResource<Track>).metadata.onFinish();
-        this.processQueue();
-      } else if (newState.status === AudioPlayerStatus.Playing) {
-        // If the Playing state has been entered, then a new track has started playback.
-        (newState.resource as AudioResource<Track>).metadata.onStart();
-      }
-    });
-
-    this.audioPlayer.on('error', error =>
-      (error.resource as AudioResource<Track>).metadata.onError(error),
-    );
-
-    voiceConnection.subscribe(this.audioPlayer);
-  }
-
-  enqueue(channel: TextChannel, query: string, title?: string, image?: string): Track {
-    const track = new Track(channel, query, title, image);
-    this.queue.push(track);
-    this.processQueue();
-    return track;
-  }
-
-  stop(options?: { skipCount?: number; force?: boolean }): number {
-    let skipped = 0;
-    if (options?.force) this.queueLock = true;
-    if (options?.skipCount) {
-      if (options?.skipCount > 1) {
-        skipped = this.queue.splice(0, options?.skipCount - 1).length;
-        skipped += this.audioPlayer.state.status === AudioPlayerStatus.Idle ? 0 : 1;
-      }
-    } else {
-      this.queue = [];
-    }
-    this.audioPlayer.stop(options?.force);
-    return skipped;
-  }
-
-  private async processQueue(): Promise<void> {
-    // If the queue is locked (already being processed), is empty, or the audio player is already playing something, return
-    if (
-      this.queueLock ||
-      this.audioPlayer.state.status !== AudioPlayerStatus.Idle ||
-      this.queue.length === 0
-    ) {
-      return;
-    }
-    // Lock the queue to guarantee safe access
-    this.queueLock = true;
-
-    // Take the first item from the queue. This is guaranteed to exist due to the non-empty check above.
-    const nextTrack = this.queue.shift()!;
-    try {
-      // Attempt to convert the Track into an AudioResource (i.e. start streaming the video)
-      const resource = await nextTrack.createAudioResource();
-      this.audioPlayer.play(resource);
-      this.queueLock = false;
-    } catch (error) {
-      // If an error occurred, try the next item of the queue instead
-      nextTrack.onError(error as Error);
-      this.queueLock = false;
-      return this.processQueue();
-    }
-  }
 }
 
 export async function musicPlay(interaction: CommandInteraction): Promise<unknown> {
@@ -388,7 +57,7 @@ export async function musicPlay(interaction: CommandInteraction): Promise<unknow
       (subscription.audioPlayer.state.status === AudioPlayerStatus.Idle &&
         subscription.queue.length === 0))
   ) {
-    subscription = new MusicSubscription(
+    subscription = new Subscription(
       joinVoiceChannel({
         channelId: channel.id,
         guildId: guild.id,
@@ -399,12 +68,10 @@ export async function musicPlay(interaction: CommandInteraction): Promise<unknow
     setSubscription(guild.id, subscription);
   }
 
-  // If there is no subscription, tell the user they need to join a channel.
   if (!subscription) {
     return interaction.followUp('Join a voice channel and then try that again.');
   }
 
-  // Make sure the connection is ready before processing the user's request
   try {
     await entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 20e3);
   } catch (error) {
