a6726024c71b18c8b91130a713afa72a1bf3cc93
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-10-02 01:47:24 +0800

    feat(soundcloud): add caching

--- src/modules/soundcloud.ts
+++ src/modules/soundcloud.ts
@@ -1,16 +1,33 @@
 import { Client, Playlist, Song } from 'soundcloud-scraper';
 const soundcloud = new Client();
 
+const _trackCache = new Map<string, Song>();
+const _playlistCache = new Map<string, Playlist>();
+
 function removeQueries(url: string): string {
   return url.split('?')[0];
 }
 
 export async function getSoundCloudTrack(url: string): Promise<Song | undefined> {
-  const song = await soundcloud.getSongInfo(removeQueries(url));
-  if (song) return song;
+  const id = removeQueries(url).trim();
+
+  const cached = _trackCache.get(id);
+  if (cached) return cached;
+
+  const song = await soundcloud.getSongInfo(id);
+  if (song) _trackCache.set(id, song);
+
+  return song;
 }
 
 export async function getSoundCloudPlaylist(url: string): Promise<Playlist | undefined> {
-  const playlist = await soundcloud.getPlaylist(removeQueries(url));
-  if (playlist) return playlist;
+  const id = removeQueries(url).trim();
+
+  const cached = _playlistCache.get(id);
+  if (cached) return cached;
+
+  const playlist = await soundcloud.getPlaylist(id);
+  if (playlist) _playlistCache.set(id, playlist);
+
+  return playlist;
 }
