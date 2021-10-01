b9fd119d57a3a74f0455d2a81863b1d360b04e6e
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-10-02 01:48:31 +0800

    feat(youtube): add caching

--- src/modules/youtube.ts
+++ src/modules/youtube.ts
@@ -1,20 +1,37 @@
 import youtubeSearch, { YouTubeSearchOptions, YouTubeSearchResults } from 'youtube-search';
+import ytdl_core from 'ytdl-core';
+const { getInfo } = ytdl_core;
 
-const _cache = new Map<string, YouTubeSearchResults>();
+const _infoCache = new Map<string, ytdl_core.videoInfo>();
+const _searchCache = new Map<string, YouTubeSearchResults>();
 
 const options: YouTubeSearchOptions = {
   key: process.env.GOOGLE_KEY,
   maxResults: 1,
 };
 
+export async function getYouTubeInfo(url: string): Promise<ytdl_core.videoInfo | undefined> {
+  const id = url.trim();
+
+  const cached = _infoCache.get(id);
+  if (cached) return cached;
+
+  const data = await getInfo(id);
+  if (data) _infoCache.set(id, data);
+
+  return data;
+}
+
 export async function searchYouTube(term: string): Promise<YouTubeSearchResults | undefined> {
-  try {
-    if (!_cache.get(term)) {
-      const data = await youtubeSearch(term, options);
-      if (data.results.length) _cache.set(term, data.results[0]);
-    }
-    return _cache.get(term);
-  } catch (error) {
-    console.error(error);
-  }
+  const id = term.trim();
+
+  const cached = _searchCache.get(id);
+  if (cached) return cached;
+
+  const data = await youtubeSearch(id, options);
+
+  const result = data.results[0];
+  if (result) _searchCache.set(id, result);
+
+  return result;
 }
