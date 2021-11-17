5ad34e30e8804d705bccefcfc15c02fb633558d1
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-11-17 13:37:04 +0800

    chore(main): update client intents

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ export const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_PRESENCES,
     Intents.FLAGS.GUILD_VOICE_STATES,
   ],
