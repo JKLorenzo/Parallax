22ec5c49131803969c1ffdc8c9a6dd0a0c90fe7f
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-09-22 07:16:14 +0800

    fix(get_ping): rename run to exec

--- src/routes/ping/get.ts
+++ src/routes/ping/get.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express';
 import APIRoute from '../../structures/api_route.js';
 
 export default class GetPing extends APIRoute {
-  run(req: Request, res: Response): void {
+  exec(req: Request, res: Response): void {
     res.send({ ping: this.bot.client.ws.ping });
   }
 }
