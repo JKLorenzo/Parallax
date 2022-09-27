5b625f8b5e04bccea2b61b96910bcebec2eadb50
JKLorenzo <keanulorenzo32@gmail.com>
2022-09-27 05:51:52 +0000

    Deploying to gh-pages from @ JKLorenzo/Parallax@5661b8264b073e716248bf70d1eddfbb43ef4545 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "7d1548a78ebe4b8145a316afb72f215b",
-"index.html": "a62ae4957aed9e929e2c465c946fe7c8",
-"/": "a62ae4957aed9e929e2c465c946fe7c8",
+"index.html": "c7bd904a94078b49d7e6e4cfba808ebc",
+"/": "c7bd904a94078b49d7e6e4cfba808ebc",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "daa9ca9fba6bc1be776290e30a453839",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
