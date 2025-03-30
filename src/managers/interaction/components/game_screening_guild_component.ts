f8becec8feaed05297e0aa61149a1c5ee25a686a
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2025-03-30 13:21:15 +0800

    feat(game_screening_guild_component): add refresh feature

--- src/managers/interaction/components/game_screening_guild_component.ts
+++ src/managers/interaction/components/game_screening_guild_component.ts
@@ -16,6 +16,7 @@ import GameManager from '../../game/game_manager.js';
 enum Id {
   Approve = 'approve',
   Deny = 'deny',
+  Refresh = 'refresh',
 }
 
 export default class GameScreeningGuildComponent extends Component {
@@ -33,6 +34,12 @@ export default class GameScreeningGuildComponent extends Component {
           {
             customId: this.makeId(Id.Deny),
             label: 'Deny',
+            type: ComponentType.Button,
+            style: ButtonStyle.Secondary,
+          },
+          {
+            customId: this.makeId(Id.Refresh),
+            label: 'Refresh',
             type: ComponentType.Button,
             style: ButtonStyle.Secondary,
           },
@@ -64,6 +71,9 @@ export default class GameScreeningGuildComponent extends Component {
         break;
       case Id.Deny:
         await this.deny(interaction, applicationId);
+        break;
+      case Id.Refresh:
+        await this.refresh(interaction, applicationId);
         break;
       default:
     }
@@ -140,6 +150,25 @@ export default class GameScreeningGuildComponent extends Component {
       roleId: roleDeleted ? undefined : guildGameData.roleId,
       moderatorId: interaction.user.id,
     });
+
+    const embed = GameManager.makeScreeningEmbed(gameData, guildGameData);
+    await interaction.update({ embeds: [embed] });
+  }
+
+  async refresh(interaction: MessageComponentInteraction<CacheType>, applicationId: string) {
+    const db = DatabaseFacade.instance();
+
+    const guild = interaction.guild;
+    if (!guild) return;
+
+    const config = await db.gameConfig(guild.id);
+    if (!config?.enabled) return;
+
+    const gameData = await db.gameData(applicationId);
+    if (!gameData) return;
+
+    const guildGameData = await db.guildGameData(guild.id, applicationId);
+    if (!guildGameData) return;
 
     const embed = GameManager.makeScreeningEmbed(gameData, guildGameData);
     await interaction.update({ embeds: [embed] });
