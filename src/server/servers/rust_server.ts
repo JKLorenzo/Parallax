d6692a0927c2a8628d27e18e879cf0d57004feb0
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2026-01-03 20:41:21 +0800

    chore(rust_server): remove unused import

--- src/server/servers/rust_server.ts
+++ src/server/servers/rust_server.ts
@@ -1,7 +1,6 @@
 import ServerManager from '../server_manager.js';
 import Utils from '../../misc/utils.js';
 import Server from '../modules/server.js';
-import type { CacheType, ChatInputCommandInteraction } from 'discord.js';
 
 export default class RustServer extends Server {
   constructor(manager: ServerManager) {
