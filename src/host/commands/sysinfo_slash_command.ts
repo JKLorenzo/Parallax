1fe6bddee7f0e46ea395e4b28ec0f985165b5eba
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2026-02-21 14:19:33 +0800

    feat: improve sysinfo data

--- src/host/commands/sysinfo_slash_command.ts
+++ src/host/commands/sysinfo_slash_command.ts
@@ -8,6 +8,8 @@ import {
 } from 'discord.js';
 import { CommandScope, SlashCommand } from '../../interaction/modules/command.js';
 import HostManager from '../host_manager.js';
+import si from 'systeminformation';
+import humanizeDuration from 'humanize-duration';
 
 export default class SysInfoSlashCommand extends SlashCommand {
   constructor() {
@@ -28,48 +30,127 @@ export default class SysInfoSlashCommand extends SlashCommand {
   }
 
   async exec(interaction: ChatInputCommandInteraction<CacheType>) {
-    const { cpu, memory } = await HostManager.instance().proxmox.status();
+    const toGB = (bytes: number) =>
+      `${`${bytes / Math.pow(1000, 3)}`.substring(0, `${bytes / Math.pow(1000, 3)}`.indexOf('.') + 2)} GB`;
 
-    const sysEmbed = new EmbedBuilder({
-      author: { name: 'Parallax Server System Information' },
-      title: 'System',
-      fields: [
-        {
-          name: 'CPU Usage',
-          value: cpu,
-          inline: true,
-        },
-        {
-          name: 'Memory Usage',
-          value: memory,
-          inline: true,
-        },
-      ],
-    });
+    const nodeStatus = await HostManager.instance().proxmox.nodeStatus();
+    const currentLoadData = await si.currentLoad();
+    const memData = await si.mem();
+
+    const embeds = [
+      new EmbedBuilder({
+        author: { name: 'System Information' },
+        title: 'Host Information',
+        fields: [
+          {
+            name: 'CPU Usage',
+            value: `${nodeStatus.cpu.usage.toFixed(2)} % at ${nodeStatus.cpu.freq} MHz`,
+            inline: true,
+          },
+          {
+            name: 'CPU Temperature',
+            value: `${nodeStatus.cpu.temp} °C`,
+            inline: true,
+          },
+          {
+            name: 'Average Load',
+            value: nodeStatus.cpu.loadAvg.map(v => `${v} %`).join(' , '),
+          },
+          {
+            name: 'GPU Usage',
+            value: `${nodeStatus.gpu.usage} %`,
+            inline: true,
+          },
+          {
+            name: 'GPU Temperature',
+            value: `${nodeStatus.gpu.temp} °C`,
+            inline: true,
+          },
+          {
+            name: 'Memory Usage',
+            value: `${Math.round((nodeStatus.memory.used / nodeStatus.memory.total) * 100)} % of ${toGB(nodeStatus.memory.total)}`,
+          },
+          {
+            name: 'Available Memory',
+            value: toGB(nodeStatus.memory.available),
+            inline: true,
+          },
+          {
+            name: 'Free Memory',
+            value: toGB(nodeStatus.memory.free),
+            inline: true,
+          },
+          {
+            name: 'SSD Temperature',
+            value: `${nodeStatus.ssd.temp.toFixed(2)} °C`,
+          },
+          {
+            name: 'System Uptime',
+            value: `${humanizeDuration(nodeStatus.uptime * 1000)}`,
+          },
+        ],
+      }),
+      new EmbedBuilder({
+        author: { name: 'System Information' },
+        title: 'Parallax Information',
+        fields: [
+          {
+            name: 'CPU Usage',
+            value: [
+              `**Core Average:** ${Math.round(currentLoadData.currentLoad)} %`,
+              ...currentLoadData.cpus.map((d, i) => `**Core ${i}:** ${Math.round(d.load)} %`),
+            ].join('\n'),
+          },
+          {
+            name: 'Memory Usage',
+            value: `${Math.round((memData.used / memData.total) * 100)} % of ${toGB(memData.total)}`,
+          },
+          {
+            name: 'Cached',
+            value: toGB(memData.buffcache),
+            inline: true,
+          },
+          {
+            name: 'Available',
+            value: toGB(memData.available),
+            inline: true,
+          },
+          {
+            name: 'Free',
+            value: toGB(memData.free),
+            inline: true,
+          },
+        ],
+      }),
+    ];
 
     const battData = HostManager.instance().ups.data;
-    const battEmbed = new EmbedBuilder({
-      author: { name: 'Parallax Server System Information' },
-      title: 'Power Supply',
-      fields: [{ name: 'State', value: battData.status.state, inline: false }],
-    });
+    if (battData) {
+      const battEmbed = new EmbedBuilder({
+        author: { name: 'System Information' },
+        title: 'UPS Information',
+        fields: [{ name: 'State', value: battData.status.state, inline: false }],
+      });
 
-    if (battData.isNormal() || battData.isPowerFail()) {
-      battEmbed.addFields([
-        { name: 'Load', value: battData.status.powerLoad, inline: true },
-        { name: 'Capacity', value: battData.status.capacity, inline: true },
-        { name: 'Runtime', value: battData.status.remainingRuntime, inline: true },
-        { name: 'Source', value: battData.status.source, inline: false },
-        { name: 'Utility', value: battData.status.voltageUtility, inline: true },
-        { name: 'Output', value: battData.status.voltageOutput, inline: true },
-        { name: 'Line Interaction', value: battData.status.lineInteraction, inline: true },
-      ]);
+      if (battData.isNormal() || battData.isPowerFail()) {
+        battEmbed.addFields([
+          { name: 'Load', value: battData.status.powerLoad, inline: true },
+          { name: 'Capacity', value: battData.status.capacity, inline: true },
+          { name: 'Runtime', value: battData.status.remainingRuntime, inline: true },
+          { name: 'Source', value: battData.status.source, inline: false },
+          { name: 'Utility', value: battData.status.voltageUtility, inline: true },
+          { name: 'Output', value: battData.status.voltageOutput, inline: true },
+          { name: 'Line Interaction', value: battData.status.lineInteraction, inline: true },
+        ]);
+      }
+
+      battEmbed.addFields([{ name: 'Last Event', value: battData.status.lastEvent, inline: true }]);
+
+      embeds.push(battEmbed);
     }
 
-    battEmbed.addFields([{ name: 'Last Event', value: battData.status.lastEvent, inline: true }]);
-
     await interaction.reply({
-      embeds: [sysEmbed, battEmbed],
+      embeds: embeds,
       flags: MessageFlags.Ephemeral,
     });
   }
--- src/host/operators/proxmox_operator.ts
+++ src/host/operators/proxmox_operator.ts
@@ -17,20 +17,34 @@ export default class ProxmoxOperator {
     });
   }
 
-  async status() {
-    const telemetry = this.telemetry.start(this.status);
+  async nodeStatus() {
+    const telemetry = this.telemetry.start(this.nodeStatus);
 
     const node = EnvironmentFacade.instance().get('proxmoxNode');
-    const vmid = EnvironmentFacade.instance().get('proxmoxVMID');
-
-    const status = await this.api.nodes.$(node).qemu.$(parseInt(vmid)).status.current.$get();
-
-    const maxmem = Number(status.maxmem);
-    const freemem = Number(status.freemem);
+    const status = await this.api.nodes.$(node).status.$get();
+    const sensors = JSON.parse(status.sensorsOutput);
 
     const data = {
-      cpu: `${Math.ceil(status.cpu)} %`,
-      memory: `${Math.ceil(((maxmem - freemem) / maxmem) * 100)} %`,
+      cpu: {
+        usage: status['cpu'] as number,
+        freq: status['cpuinfo']['mhz'] as string,
+        loadAvg: status['loadavg'] as string[],
+        temp: sensors['k10temp-pci-00c3']['Tccd1']['temp3_input'] as number,
+      },
+      memory: {
+        used: status['memory']['used'] as number,
+        total: status['memory']['total'] as number,
+        free: status['memory']['free'] as number,
+        available: status['memory']['available'] as number,
+      },
+      gpu: {
+        usage: sensors['nouveau-pci-0600']['GPU core']['in0_input'] as number,
+        temp: sensors['nouveau-pci-0600']['temp1']['temp1_input'] as number,
+      },
+      ssd: {
+        temp: sensors['nvme-pci-0100']['Composite']['temp1_input'] as number,
+      },
+      uptime: status['uptime'] as number,
     };
 
     telemetry.end();
